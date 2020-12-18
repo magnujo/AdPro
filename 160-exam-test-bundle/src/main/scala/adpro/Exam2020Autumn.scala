fc2ecc6f726a737d0148e775121c60ff7857594e
Andrzej Wąsowski <wasowski.andrzej@gmail.com>
2020-12-18 18:39:35 +0100

    Demo setup for the final exam

---
+++ 160-exam-test-bundle/src/main/scala/adpro/Exam2020Autumn.scala
@@ -0,0 +1,72 @@
+/************************************************************************
+ * Final Exam: Advanced Programming by Andrzej Wąsowski
+ * IT University of Copenhagen, Autumn 2020: 6 January 2021, 9:00
+ *
+ * Your Full Name: ___
+ * Your ITU email account: ___
+ *
+ * The exam consists of 9 questions to be solved within 4 hours.
+ *
+ * It is possible  to answer later questions, missing  the answers for
+ * the previous ones, but it is recommended to answer in order.
+ *
+ * You can use  any function from the course  (textbook, exercises) in
+ * the  solutions, as  well as  standard library  functions.  You  can
+ * access any written or electronic material, also online, but you are
+ * not allowed to communicate with anybody during the exam.
+ *
+ * By  submitting, you  declare  to have  solved  the problems  alone,
+ * without communicating with anybody.
+ *
+ * SUBMISSION
+ *
+ * Solve the tasks in the file 'Exam2020Autumn.scala' (this file) found
+ * in the zip archive made available on LearnIt.
+ *
+ * Fill in your name and your ITU email above, in the top of the file.
+ *
+ * Submit this file and only this  file to learnIT.  Do not convert to
+ * any  other  format than  .scala.   Do  not  submit the  entire  zip
+ * archive. Do not  reorder the  answers, and  do not  remove question
+ * numbers from the file.  The only accepted file format is '.scala'.
+ *
+ * Keep the solutions within 80 columns width to make grading easier.
+ *
+ * ADVICE
+ *
+ * The  answers  will   be  graded  manually. We  will   focus  on  the
+ * correctness of ideas and the use  of the course concepts. We will be
+ * permissive on  minor issues  such as semicolons,  other punctuation,
+ * small deviations  in function  names, switching between  curried and
+ * not  curried arguments,  etc.  We  will not  check whether  the type
+ * inference succeeds.   It suffices  that a  human reader  could infer
+ * types.
+ *
+ * We do not recommend solving questions to the point when they compile
+ * and pass tests.  Dependency problems  and other technical issues can
+ * take a lot of time, so only do this, once you are done with drafting
+ * all answers.
+ *
+ * Nevertheless, if  you do compile,  you can use the  'build.sbt' file
+ * provided  in the  zip  archive linked  above. It  has the  necessary
+ * library dependencies  configured. The zip archive also  contains the
+ * course libraries that the solutions depend on.
+ *
+ * The  percentage  at  the   beginning  of  each  question  indicates
+ * difficulty of the question, and will be used as guide in grading.
+ *
+ * Good luck!
+ */
+
+package adpro
+
+import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary._
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks._
+import org.scalatest.matchers.should.Matchers._
+import org.scalactic.Equality
+
+
+object Q1 { // here questions will start
+
+}
